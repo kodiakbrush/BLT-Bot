8ea30890f21b6cdf6d09b917aa808ebfaac67b19
jdlowman2 <lowmanj@mit.edu>
2015-01-21 16:50:51 -0500

    toggle switch fixes
    blah blah

--- test_motor_edison/backward_function/backward_function.ino
+++ test_motor_edison/backward_function/backward_function.ino
@@ -12,8 +12,7 @@ SNS_A   = A0;
 
 //set up for running motor A
 void setup() {
-  setPwmSwizzler(3,5,10,11);
-  // Configure the A output
+  setPwmSwizzler(3, 5, 10, 11);  // Configure the A output
   pinMode(BRAKE_A, OUTPUT);  // Brake pin on channel A
   pinMode(DIR_A, OUTPUT);    // Direction pin on channel A
 
@@ -21,19 +20,6 @@ void setup() {
   Serial.begin(9600);
 }
 
-// -----------------FUNCTION loop for running motor A backward-------------------- //
-
-void Backward(int time_back) // Set the outputs to run the motor backward
-{
-  digitalWrite(BRAKE_A, LOW);  // setting againg the brake LOW to disable motor brake
-  digitalWrite(DIR_A, LOW);    // now change the direction to backward setting LOW the DIR_A pin
-  digitalWrite(PWM_A, 255);  // runs the motor at full speed
-
-  delay (time_back);
-
-  analogWrite(PWM_A, 0); // turn off power to motor A
-  digitalWrite(BRAKE_A, HIGH); // raise the brake 
-}
 
 
 // ------------------- MAIN LOOP ---------------------------- //
@@ -41,3 +27,17 @@ void loop()
 {
   Backward(0);
 }
+// -----------------FUNCTION loop for running motor A backward-------------------- //
+
+void Backward(int time_back) // Set the outputs to run the motor backward
+{
+  digitalWrite(BRAKE_A, LOW);  // setting againg the brake LOW to disable motor brake
+  digitalWrite(DIR_A, LOW);    // now change the direction to backward setting LOW the DIR_A pin
+  analogWrite(PWM_A, 255);  // runs the motor at full speed- should be analogWrite/////////////////////////
+
+  delay (time_back);
+
+  analogWrite(PWM_A, 0); // turn off power to motor A
+  digitalWrite(BRAKE_A, HIGH); // raise the brake 
+}
+
